fc76f730f01feccb2cac7c9da4b6c33e8dfab218
zniedl-bbc <Luca.Niederer@bbcag.ch>
2016-11-16 10:31:06 +0100

    search

--- ejbModule/ch/bbc/rottengold/ejb/SearchBean.java
+++ ejbModule/ch/bbc/rottengold/ejb/SearchBean.java
@@ -29,11 +29,17 @@ public class SearchBean implements SearchBeanLocal {
 	@Override
 	public Website[] searchWebsite(String searchInput) {
 		@SuppressWarnings("unchecked")
-		List<Website> results = em.createNamedQuery("Website.findByWebsiteName").setParameter("webName", searchInput)
-				.getResultList();
-		searchResults = new Website[results.size()];
-		searchResults = (Website[]) results.toArray(); 
-		
+		List<Website> results = em.createNamedQuery("Website.findByWebsiteName")
+				.setParameter("webName", "%" + searchInput + "%").getResultList();
+		int listSize = results.size();
+		searchResults = new Website[listSize];
+
+		int counter = 0;
+		while (listSize > counter) {
+			searchResults[counter] = results.get(counter);
+			counter++;
+		}
+
 		return searchResults;
 	}
 
